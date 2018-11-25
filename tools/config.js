4e44d839201fd8aeefe5930b03a64f4ff6bc867c
leonardo macbook <support@crimatex.com>
2018-11-25 15:10:49 -0200

    trocando porta de acesso

--- tools/config.js
+++ tools/config.js
@@ -2,8 +2,8 @@
 
 var Config = {
 
-	PORT : process.env.PORT || 8080,
-	HOST : '127.0.0.1',
+	PORT : process.env.PORT || 9093,
+	HOST : '0.0.0.0',
 	WEBSOCKET_PORT : 4000,
 	GATEWAY_QRPAGUE_BACKEND : 'http://204.48.27.105:9092'
   
