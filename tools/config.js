3cf498ab2716c40455e4801ccbe14043aec35bdb
leonardoSilveira <leo.compiler@gmail.com>
2018-11-26 14:18:23 -0200

    acertando  arquivo config.js

--- tools/config.js
+++ tools/config.js
@@ -1,11 +1,11 @@
 
 
 var Config = {
-
-	PORT : process.env.PORT || 9093,
+	PROTOCOL: 'http',
+	PORT :  9093,
 	HOST : '0.0.0.0',
 	WEBSOCKET_PORT : 4000,
-	GATEWAY_QRPAGUE_BACKEND : 'http://204.48.27.105:9092'
+	GATEWAY_QRPAGUE_BACKEND : 'http://'+Config.HOST+':9092'
   
 };
 
