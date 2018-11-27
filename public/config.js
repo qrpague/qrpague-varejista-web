e95c9a163e7de3050dc4bf095f5a0052e7cf68ec
leonardoSilveira <leo.compiler@gmail.com>
2018-11-27 11:26:56 -0200

    acertando variaveis

--- public/config.js
+++ public/config.js
@@ -15,13 +15,13 @@ var connectApp =
   };
 
 
-  // connectApp.protocol = 'http';
-  // connectApp.host = '204.48.27.105';
-  // connectApp.port = '9093';
-  // connectApp.websocket_url = 'ws://204.48.27.105:4000';
+  connectApp.protocol = 'http';
+  connectApp.host = '204.48.27.105';
+  connectApp.port = '9093';
+  connectApp.websocket_url = 'ws://204.48.27.105:4000';
 
   connectApp.idTerminal = '0001128322332'
-  connectApp.protocol = 'http';
-  connectApp.host = '0.0.0.0';
-  connectApp.port = '9093';
-  connectApp.websocket_url = 'ws://0.0.0.0:4000';
+  // connectApp.protocol = 'http';
+  // connectApp.host = '0.0.0.0';
+  // connectApp.port = '9093';
+  // connectApp.websocket_url = 'ws://0.0.0.0:4000';
