561d3f61d20888b3155e51cdbf6b5527fc7414bc
leonardoSilveira <leo.compiler@gmail.com>
2018-12-03 15:36:59 -0200

    url

--- public/config.js
+++ public/config.js
@@ -15,10 +15,10 @@ var connectApp =
   };
 
 
-  // connectApp.protocol = 'http';
-  // connectApp.host = '204.48.27.105';
-  // connectApp.port = '9093';
-  // connectApp.websocket_url = 'ws://204.48.27.105:4000';
+  connectApp.protocol = 'http';
+  connectApp.host = '204.48.27.105';
+  connectApp.port = '9093';
+  connectApp.websocket_url = 'ws://204.48.27.105:4000';
 
   connectApp.idTerminal = '0001128322332'
   connectApp.terminal = {
@@ -29,8 +29,8 @@ var connectApp =
     "latitudeTerminal": "-15.7801",
     "longitudeTerminal": "-47.9292"
   },
-  connectApp.protocol = 'http';
-  connectApp.host = '0.0.0.0';
-  connectApp.port = '9093';
-  connectApp.websocket_url = 'ws://0.0.0.0:4000';
+  // connectApp.protocol = 'http';
+  // connectApp.host = '0.0.0.0';
+  // connectApp.port = '9093';
+  // connectApp.websocket_url = 'ws://0.0.0.0:4000';
   connectApp.image_shared_url = 'http://204.48.27.105:9093/';
